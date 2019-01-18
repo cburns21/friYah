8b81ab225fadb9fc049140d81f11a578faec7972
caroline burns <carolineburns@carolines-MacBook.local>
2019-01-18 13:01:40 -0700

    made data, made app, and finished everything besides deploying

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3210
+const data = require('./data.json')
+
+app.use(express.static('public'))
+
+app.get('/data', (req, res, next) => {
+    res.status(200).send({
+        "message": 'success!',
+        "data": data
+    })
+})
+
+
+app.get('/:results', (req, res, next) => {
+    const result = req.params.result
+    if(!data.results.includes(result)){
+        res.status(404).send('SHAME!')
+    }
+    else {
+        const final = data.results.filter(info => info.girl.includes(result))
+        res.status(200).send(final)}
+})
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
+
+app.listen(port, () => console.log(`Porty on ${port}`))
